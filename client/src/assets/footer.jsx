4ffdcaf6681db15e8f35f988c1fa41bbf331c5f5
tapan0p <mahatatapan2000@gmail.com>
2024-04-21 10:47:06 +0530

    final update

--- client/src/assets/footer.jsx
+++
@@ -1,9 +0,0 @@
-let Footer=()=>{
-    return (
-        <div className="flex justify-center items-center w-full h-10 bg-pink-200">
-            @Medical AI chatbot Team NLP squad 2024
-        </div>
-    )
-}
-
-export default Footer;
